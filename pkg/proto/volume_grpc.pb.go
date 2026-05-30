28401d219178c91d46d29a13748027f0402ddab6
John Buluba <buluba89@gmail.com>
2026-05-31 02:11:39 +0300

    feat(proto): add VolumeService.Resolve RPC (referral seam)
    Resolve(name) -> {location}. Empty location means the server serves the volume
    locally; a non-empty value is a referral the client reconnects to.

--- pkg/proto/volume_grpc.pb.go
+++ pkg/proto/volume_grpc.pb.go
@@ -19,7 +19,8 @@ import (
 const _ = grpc.SupportPackageIsVersion9
 
 const (
-	VolumeService_List_FullMethodName = "/gmountie.VolumeService/List"
+	VolumeService_List_FullMethodName    = "/gmountie.VolumeService/List"
+	VolumeService_Resolve_FullMethodName = "/gmountie.VolumeService/Resolve"
 )
 
 // VolumeServiceClient is the client API for VolumeService service.
@@ -27,6 +28,7 @@ const (
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type VolumeServiceClient interface {
 	List(ctx context.Context, in *VolumeListRequest, opts ...grpc.CallOption) (*VolumeListReply, error)
+	Resolve(ctx context.Context, in *VolumeResolveRequest, opts ...grpc.CallOption) (*VolumeResolveReply, error)
 }
 
 type volumeServiceClient struct {
@@ -47,11 +49,22 @@ func (c *volumeServiceClient) List(ctx context.Context, in *VolumeListRequest, o
 	return out, nil
 }
 
+func (c *volumeServiceClient) Resolve(ctx context.Context, in *VolumeResolveRequest, opts ...grpc.CallOption) (*VolumeResolveReply, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(VolumeResolveReply)
+	err := c.cc.Invoke(ctx, VolumeService_Resolve_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // VolumeServiceServer is the server API for VolumeService service.
 // All implementations must embed UnimplementedVolumeServiceServer
 // for forward compatibility.
 type VolumeServiceServer interface {
 	List(context.Context, *VolumeListRequest) (*VolumeListReply, error)
+	Resolve(context.Context, *VolumeResolveRequest) (*VolumeResolveReply, error)
 	mustEmbedUnimplementedVolumeServiceServer()
 }
 
@@ -64,6 +77,9 @@ type UnimplementedVolumeServiceServer struct{}
 
 func (UnimplementedVolumeServiceServer) List(context.Context, *VolumeListRequest) (*VolumeListReply, error) {
 	return nil, status.Error(codes.Unimplemented, "method List not implemented")
+}
+func (UnimplementedVolumeServiceServer) Resolve(context.Context, *VolumeResolveRequest) (*VolumeResolveReply, error) {
+	return nil, status.Error(codes.Unimplemented, "method Resolve not implemented")
 }
 func (UnimplementedVolumeServiceServer) mustEmbedUnimplementedVolumeServiceServer() {}
 func (UnimplementedVolumeServiceServer) testEmbeddedByValue()                       {}
@@ -104,6 +120,24 @@ func _VolumeService_List_Handler(srv interface{}, ctx context.Context, dec func(
 	return interceptor(ctx, in, info, handler)
 }
 
+func _VolumeService_Resolve_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(VolumeResolveRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(VolumeServiceServer).Resolve(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: VolumeService_Resolve_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(VolumeServiceServer).Resolve(ctx, req.(*VolumeResolveRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // VolumeService_ServiceDesc is the grpc.ServiceDesc for VolumeService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -114,6 +148,10 @@ var VolumeService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "List",
 			Handler:    _VolumeService_List_Handler,
+		},
+		{
+			MethodName: "Resolve",
+			Handler:    _VolumeService_Resolve_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
