0628fd791e4aa5e651c92e9b42790fd3aa8887fd
John Buluba <buluba89@gmail.com>
2026-06-20 21:13:58 +0300

    cgofs: io.Attr/StatFs -> cgofuse Stat_t/Statfs_t with IDRewriter

---
+++ pkg/client/io/cgofs/attr.go
@@ -0,0 +1,37 @@
+//go:build darwin || cgofuse
+
+package cgofs
+
+import (
+	cgofuse "github.com/winfsp/cgofuse/fuse"
+	gio "go.gmountie.dev/gmountie/pkg/client/io"
+)
+
+// fillStat maps an io.Attr to a cgofuse Stat_t, applying the mount's
+// IDRewriter so the caller sees local display uid/gid (mirrors
+// setAttrFromBackend in node.go). rw may be nil (identity transform).
+func fillStat(dst *cgofuse.Stat_t, a *gio.Attr, rw *gio.IDRewriter) {
+	dst.Ino = a.Ino
+	dst.Size = int64(a.Size)
+	dst.Blocks = int64(a.Blocks)
+	dst.Mode = a.Mode
+	dst.Nlink = a.Nlink
+	dst.Rdev = uint64(a.Rdev)
+	dst.Blksize = int64(a.Blksize)
+	dst.Atim = cgofuse.Timespec{Sec: int64(a.Atime), Nsec: int64(a.Atimensec)}
+	dst.Mtim = cgofuse.Timespec{Sec: int64(a.Mtime), Nsec: int64(a.Mtimensec)}
+	dst.Ctim = cgofuse.Timespec{Sec: int64(a.Ctime), Nsec: int64(a.Ctimensec)}
+	dst.Uid, dst.Gid = rw.Inbound(a.Uid, a.Gid)
+}
+
+// fillStatfs maps an io.StatFs to a cgofuse Statfs_t.
+func fillStatfs(dst *cgofuse.Statfs_t, s *gio.StatFs) {
+	dst.Bsize = uint64(s.Bsize)
+	dst.Frsize = uint64(s.Frsize)
+	dst.Blocks = s.Blocks
+	dst.Bfree = s.Bfree
+	dst.Bavail = s.Bavail
+	dst.Files = s.Files
+	dst.Ffree = s.Ffree
+	dst.Namemax = uint64(s.Namelen)
+}
